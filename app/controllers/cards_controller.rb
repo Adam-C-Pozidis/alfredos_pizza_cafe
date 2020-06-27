089d43f9ff584a058383fc14c23f453a79e678ff
Adam Pozidis <adchrpoz@gmail.com>
2020-06-27 12:35:24 +0200

    menu page nearly done

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,6 +4,16 @@ class CardsController < ApplicationController
   def create
     @shopping_cart = current_shopping_cart
     @card = @shopping_cart.cards.new(card_params)
+
+    # if @shopping_cart.cards.include?(Card.find(@card.menu_item.id))
+
+
+    # if @shopping_cart_cards.include?(@card)
+    #   @card.quantity = += Card.find.quantity
+    #   @card.update_attributes(:quantity => )
+    # else
+    #   @card.save
+    # end
     @card.save
     session[:shopping_cart_id] = @shopping_cart.id
   end
@@ -15,7 +25,8 @@ class CardsController < ApplicationController
     @card.update_attributes(card_params)
     @card.destroy if @card.quantity == 0
     @shopping_cart_cards = current_shopping_cart.cards
-    redirect_to shopping_carts_path
+
+
   end
 
   def destroy
@@ -23,7 +34,7 @@ class CardsController < ApplicationController
     @card = @shopping_cart.cards.find(params[:id])
     @card.destroy
     @shopping_cart_cards = current_shopping_cart.cards
-    redirect_to shopping_carts_path
+
   end
 
   private
